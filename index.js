331dec52bfed0b80159dbcf599748c1c754b7121
aman0532005-hue <aman0532005@gmail.com>
2026-07-27 10:18:47 +0530

    callback

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log("hello world ")
+// console.log("hello world ")
+// console.log("using js")
+// var a = 34;
+// console.log("a = " + a)
+// if (a > 10) {
+//     var a=40;
+//     console.log("hi a insidethe block = " + a )
+// }
+// console.log("hi a outside the block = " + a )
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(230,30))
+
+// const sum = (a,b) => {
+//     return a+b;
+// }
+// console.log(sum(12,30))
+
+// const date=function(msg){
+//     return "hello,i am aman"+msg;
+// }
+// data(" and node")
+
+//iife
+// (()=>{
+//     console.log("hello i am aman")
+// })()
+
+//callback fn
+// function sum(a,b){
+//     return a+b;
+// }
+// function sumWithMsg(clbk,msg){
+//     const result = clbk(2,6);
+//     console.log("hey,your result is " + result + msg  )
+// }
+// sumWithMsg(sum," and i am aman")
+
+function login(msg,error){
+    if(error){
+        console.log("error is " + error)
+    }else{
+        console.log("msg is " + msg)
+    }
+}
+function loginhandler(username,password,callback){
+    if(username == "ptomer40" && password == "123"){
+        callback(" success",null)
+    }else{
+        callback(null,"login failed")
+    }
+}
+loginhandler("ptomer0","123",login)
